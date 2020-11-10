2b44797af0cc842a428bd620e74de36d05cafba4
aakash2809 <aakashrajak289@gmail.com>
2020-11-10 19:34:44 +0530

    initial commit

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
 
 public class TicTacToeGame {
+	// INITIALIZATION GAME BOARD
+	public static char[] initializeBoard() {
+		char[] gameBoard = new char[10];
+		for (int index = 1; index < gameBoard.length; index++) {
+			gameBoard[index] = ' ';
+		}
+		return gameBoard;
+
+	}
+
 	// MAIN METHOD
 	public static void main(String[] args) {
-		System.out.println("Welcome to Tic Tac Toe ");
+		System.out.println("       Welcome to Tic Tac Toe      ");
+		System.out.println("---------------------------------------");
+		// CALLING METHOD FOR THE PURPOSE OF UC1
+		char[] intializedBoard = initializeBoard();
+
 	}
+
 }
