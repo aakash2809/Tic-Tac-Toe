f774db5773d39a34d24d8693c412731fcb95c172
aakash2809 <aakashrajak289@gmail.com>
2020-11-10 19:48:04 +0530

    Add : input to for user and computer

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -11,13 +11,51 @@ public class TicTacToeGame {
 
 	}
 
-	// MAIN METHOD
-	public static void main(String[] args) {
-		System.out.println("       Welcome to Tic Tac Toe      ");
-		System.out.println("----------------------------------------");
-		// CALLING METHOD FOR THE PURPOSE OF UC1
-		char[] intializedBoard = initializeBoard();
+	// have your choice
+	public static int haveYourChoice1() {
+
+		int result = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter your choice either O OR X  : ");
+		System.out.println("for termination press : T");
+		char option = sc.next().charAt(0);
+
+		while (true) {
+
+			switch (option) {
+			case 'O':
+				result = 1;
+				return result;
+			case 'X':
+				result = 0;
+				return result;
+			case 'T':
+				System.exit(1);
+
+			default:
+				System.out.println("wrong choice");
+
+			}
+
+		}
 
 	}
 
+	// MAIN METHOD
+	public static void main(String[] args) {
+		System.out.println("       Welcome to Tic Tac Toe      ");
+		System.out.println("---------------------------------------");
+		// CALLING METHOD FOR THE PURPOSE OF UC1
+		char[] intializedBoard = initializeBoard();
+		// CALLING METHOD FOR THE PURPOSE OF UC2
+		int userChoice = haveYourChoice1();
+		if (userChoice == 1) {
+			System.out.println("Computer choice is " + 'X');
+		} else {
+			System.out.println("Computer choice is " + 'O');
+
+		}
+	}
+
 }
+
